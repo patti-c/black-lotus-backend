f1a579e8ef549dc194decd415210cf5712f421c1
Alexandra Pick-Aluas <alex@Alexandras-MBP.home>
2018-12-18 17:05:41 -0500

    CORS changes

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,13 @@ Bundler.require(*Rails.groups)
 
 module BlackLotus
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins /^lotusdeckbuilder.herokuapp.com.*/
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.before_configuration do
